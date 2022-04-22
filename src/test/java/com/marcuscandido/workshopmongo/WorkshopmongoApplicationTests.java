361cc53c9791a5a86886e1440e8df82d7b0b39fc
MarVinCan <marcus.v.e.candido@gmail.com>
2022-04-22 14:46:52 -0300

    Primeiro commit - projeto criado

---
+++ src/test/java/com/marcuscandido/workshopmongo/WorkshopmongoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.marcuscandido.workshopmongo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkshopmongoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
